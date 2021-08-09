74597394a2c1675a64ab3a4460b1c83348b5ea88
StaticShock93 <74117562+StaticShock93@users.noreply.github.com>
2021-08-09 15:17:59 -0700

    set limits to input values
    set limits from 0-100

--- js/site.js
+++ js/site.js
@@ -2,25 +2,30 @@
 // starts or controller function
 function getValues() {
     // get values from the page
-    let startValue = document.getElementById("startValue").value.min;
-    let endValue = document.getElementById("endValue").value.max;
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
     // we need to validate our input
-    // parse into Integers
-    startValue = parseInt(startValue);
-    endValue = parseInt(endValue);
+    // parse into Integers and only accept #'s from 0-100
+    startValue = Math.min(Math.max(parseInt(startValue), 0), 100);
+    endValue = Math.min(Math.max(parseInt(endValue), 0), 100);
+
 
     if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
         // we call generateNumbers
         let numbers = generateNumbers(startValue, endValue);
+
         // we call displayNumbers
         displayNumbers(numbers);
 
+
     } else {
-        alert("You must enter integers between 0 and 100");
+        alert("You must enter integers only");
     }
 
 
+
 }
+
 
 // generate numbers from startValue to the endValue
 // logic function(s)
@@ -32,7 +37,7 @@ function generateNumbers(sValue, eValue) {
     // we want to get all numbers from start to end
     for (let index = sValue; index <= eValue; index++) {
 
-        // this will execute in a loop intil index = eValue
+        // this will execute in a loop until index = eValue
         numbers.push(index);
 
     }
