28fea480c90cd07adf32be5bd6f6b1856ca66cbb
StaticShock93 <74117562+StaticShock93@users.noreply.github.com>
2021-08-24 14:24:29 -0700

    updated summary

--- js/site.js
+++ js/site.js
@@ -8,20 +8,15 @@ function getValues() {
     // parse into Integers and only accept #'s from 0-100
     startValue = Math.min(Math.max(parseInt(startValue), 0), 100);
     endValue = Math.min(Math.max(parseInt(endValue), 0), 100);
-    
+
     if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
         // we call generateNumbers
         let numbers = generateNumbers(startValue, endValue);
-
         // we call displayNumbers
         displayNumbers(numbers);
-
-
     } else {
         alert("You must enter integers only");
     }
-
-
 }
 
 
@@ -30,16 +25,12 @@ function getValues() {
 function generateNumbers(sValue, eValue) {
 
     let numbers = [];
-
-
     // we want to get all numbers from start to end
     for (let index = sValue; index <= eValue; index++) {
 
         // this will execute in a loop until index = eValue
         numbers.push(index);
-
     }
-
     return numbers;
 }
 
@@ -53,7 +44,6 @@ function displayAlert() {
         this.value = 0;
         alert("You must enter a number from 0-100");
     }
-
     if (endValue < 0 || endValue > 100) {
         this.value = 100;
         alert("You must enter a number from 0-100");
@@ -79,7 +69,6 @@ function displayNumbers(numbers) {
         templateRows += `<tr><td class="${className}">${number}</td></tr>`;
 
     }
-
     document.getElementById("results").innerHTML = templateRows;
 
 }
